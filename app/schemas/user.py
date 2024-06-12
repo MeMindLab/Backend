d71b53e2a1d0d972618aab4ee7d84aaac3cb681d
Luceta <57941164+Luceta@users.noreply.github.com>
2024-06-13 01:44:36 +0900

    feat: fix sms verify response, add schema pydantic

--- app/schemas/user.py
+++ app/schemas/user.py
@@ -1,6 +1,7 @@
+from enum import Enum
 from pydantic import BaseModel, validator, Field
 from datetime import datetime
-from typing import Optional
+from typing import Optional, Dict
 from app.models.user import UserRole
 
 
@@ -44,3 +45,22 @@ class Token(BaseModel):
     refresh_token: Optional[str]
     expires_in: int
 
+
+class Channel(str, Enum):
+    SMS = "sms"
+    CALL = "call"
+    EMAIL = "email"
+    WHATSAPP = "whatsapp"
+    SNA = "sna"
+
+
+class VerificationCheckResponse(BaseModel):
+    to: Optional[str]
+    channel: Channel
+    status: Optional[str]
+    valid: Optional[bool]
+
+
+class VerificationResult(BaseModel):
+    success: bool
+    data: Dict[str, VerificationCheckResponse]
