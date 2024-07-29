574322dc2e983df1bf91664464ea0e40a80bea86
Luceta <57941164+Luceta@users.noreply.github.com>
2024-07-30 01:43:09 +0900

    feat: user withdrawal soft delete option

--- app/models/user.py
+++ app/models/user.py
@@ -1,9 +1,21 @@
+from datetime import datetime
 from uuid import uuid4
 from typing import TYPE_CHECKING
-from sqlalchemy import Column, String, Enum, Boolean, Uuid
+
+
 from enum import Enum as PythonEnum
 
-from sqlalchemy.orm import relationship, mapped_column, Mapped
+from sqlalchemy import String, Enum, Boolean, Uuid, DateTime, event
+from sqlalchemy.orm import (
+    relationship,
+    mapped_column,
+    Mapped,
+    Session,
+    ORMExecuteState,
+    with_loader_criteria,
+)
+
+
 from .common import CommonModel
 
 
@@ -16,7 +28,21 @@ class UserRole(str, PythonEnum):
     admin = "admin"
 
 
-class User(CommonModel):
+class SoftDeleteMixin:
+    deleted_at = mapped_column(DateTime(timezone=True), nullable=True)
+
+    def delete(self):
+        if self.deleted_at is None:
+            self.deleted_at = datetime.utcnow()
+
+    def undelete(self):
+        self.deleted_at = None
+
+    def is_deleted(self):
+        return self.deleted_at is not None
+
+
+class User(CommonModel, SoftDeleteMixin):
     __tablename__ = "users"
     id = mapped_column(Uuid, primary_key=True, index=True, default=uuid4)
     email: Mapped[str] = mapped_column(String(120), unique=True, index=True)
@@ -25,6 +51,7 @@ class User(CommonModel):
     role: Mapped[UserRole] = mapped_column(Enum(UserRole), default=UserRole.user)
     is_verified: Mapped[bool] = mapped_column(Boolean, default=False)
     mobile: Mapped[str | None] = mapped_column(String(120), nullable=True)
+    delete_reason: Mapped[str | None] = mapped_column(String(255), nullable=True)
 
     lemons: Mapped["Lemon"] = relationship(
         "Lemon", back_populates="user", uselist=False
@@ -35,3 +62,20 @@ class User(CommonModel):
             return f"<User(email={self.email}, nickname={self.nickname}, role={self.role.value}, is_verified={self.is_verified}, lemons={self.lemons.lemon_count})>"
         else:
             return f"<User(email={self.email}, nickname={self.nickname}, role={self.role.value}, is_verified={self.is_verified}, lemons=None)>"
+
+
+@event.listens_for(Session, "do_orm_execute")
+def apply_soft_delete_filter(state: ORMExecuteState):
+    if not state.is_select:
+        return
+
+    if state.is_select and not state.statement.get_execution_options().get(
+        "include_deleted"
+    ):
+        state.statement = state.statement.options(
+            with_loader_criteria(
+                SoftDeleteMixin,
+                lambda cls: cls.deleted_at.is_(None),
+                include_aliases=True,
+            )
+        )
